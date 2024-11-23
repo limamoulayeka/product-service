31ed9ba8808769cf85df88620a06fd971faddf5f
Limamou Laye Ka <limamoulayeka7@gmail.com>
2024-11-23 10:37:16 -0500

    feat: add product controller create method

--- src/main/java/com/limamoulayeka/product_service/controller/ProductController.java
+++ src/main/java/com/limamoulayeka/product_service/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.limamoulayeka.product_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.limamoulayeka.product_service.dto.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+
+    }
 }
