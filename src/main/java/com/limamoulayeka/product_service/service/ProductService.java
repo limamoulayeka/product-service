c6c88d51266629b657e1bc082d761d609cee1132
Limamou Laye Ka <limamoulayeka7@gmail.com>
2024-11-23 11:57:15 -0500

    feat: add product service

---
+++ src/main/java/com/limamoulayeka/product_service/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.limamoulayeka.product_service.service;
+
+import com.limamoulayeka.product_service.dto.ProductRequest;
+import com.limamoulayeka.product_service.model.Product;
+import com.limamoulayeka.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
